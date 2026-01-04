73be0af8e0d1eba50b20622fa0dc0316f2cf0a40
albinjoby <albinjobyponnu@gmail.com>
2026-01-04 22:47:02 +0530

    add server logic

---
+++ server/index.ts
@@ -0,0 +1,22 @@
+import http from "http"
+import {Server} from "socket.io"
+
+const server = http.createServer()
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+})
+
+io.on('connection',(socket)=>{
+  console.log('a user connected')
+  
+  socket.on('message',(message)=>{
+    console.log(message)
+    io.emit('message',message)
+  })
+})
+
+server.listen(3000,()=>{
+  console.log('server running on http://localhost:3000')
+})
