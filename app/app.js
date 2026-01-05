1ad01d10b160b9518e17f3c4407f76a947dc1142
albinjoby <albinjobyponnu@gmail.com>
2026-01-05 11:56:40 +0530

    add styling

--- app/app.js
+++ app/app.js
@@ -5,22 +5,27 @@ const list = document.querySelector("ul");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 
-socket.on("message", (text) => {
+socket.on("message", ({ text, senderId}) => {
   const el = document.createElement("li");
+  el.style.listStyleType = 'none'
   el.innerHTML = text;
+  
+  if(senderId === socket.id){
+    el.classList = 'me'
+  }else{
+    el.className = 'others'
+  }
+  
   list.appendChild(el);
 });
 
 function sendMessage() {
+  if(!input.value.trim()) return
   socket.emit("message", input.value);
   input.value = "";
 }
 
-button.onclick = () => {
-  if (input.value.trim()) {
-    sendMessage();
-  }
-};
+button.onclick = sendMessage 
 
 input.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
